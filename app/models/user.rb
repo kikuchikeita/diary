c51bdc8083a7493543d5d916828a5d4481bd484b
kikuchikeita <imnotpooh@gmail.com>
2020-03-20 18:04:39 +0900

    add login

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_one_attached :avatar
+  validates :name,          presence: true,   length: { maximum: 50 }
+  def avatar_check
+    if avatar.present?
+      unless avatar.content_type.in?(%(image/jpeg image/png))
+        errors.add(:avatar, 'にはjpegまたはpngファイルを添付してください')
+      end
+    else
+      unless avatar.attached?
+        errors.add(:avatar, 'ファイルを添付してください')
+      end
+    end
+  end
+
+end
